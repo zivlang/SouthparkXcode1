c2994b698ca570629c3e48b032db402bff9beceb
ziv <ziv_lang@walla.com>
2020-02-24 05:41:50 -0800

    Initial Commit

---
+++ Southpark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Southpark
+//
+//  Created by Hackeru_Student on 2/24/20.
+//  Copyright © 2020 ziv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
